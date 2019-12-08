2bf6ae6af7efa246ea8d39115fcc6d66a6540adf
Wendel Nascimento <wendel.nascimento2@outlook.com>
2019-12-08 04:00:17 -0300

    feat: base css and router config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import MapView from './screens/MapView/MapView';
+
+import MenuContext from './context/MenuContext';
 
 function App() {
+  const [menuOpened, setMenuOpened] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MenuContext.Provider value={{ opened: menuOpened, toggleMenu: () => setMenuOpened(!menuOpened) }}>
+        <Switch>
+          <Route path="/map" exact component={MapView} />
+          <Redirect to="/map" />
+        </Switch>
+      </MenuContext.Provider>
+    </BrowserRouter>
   );
 }
 
