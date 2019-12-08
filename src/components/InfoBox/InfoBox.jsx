a90b4bc77623545a05d9c849c1e0061b69f06315
Wendel Nascimento <wendel.nascimento2@outlook.com>
2019-12-08 09:43:01 -0300

    feat: adding label and link to report

--- src/components/InfoBox/InfoBox.jsx
+++ src/components/InfoBox/InfoBox.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import moment from 'moment';
+import { Icon, Row } from 'antd';
 
 import MenuContext from '../../context/MenuContext';
 
-import { InfoBoxContainer, InfoBoxDate, InfoBoxTemperature, InfoBoxText } from './style';
+import { InfoBoxContainer, InfoBoxDate, InfoBoxTemperature, InfoBoxText, InfoBoxTextBottom, ReportLink } from './style';
 
 const InfoBox = () => (
   <MenuContext.Consumer>
@@ -26,6 +27,14 @@ const InfoBox = () => (
           <InfoBoxText>
             Clima: <strong>Chuva</strong>
           </InfoBoxText>
+          <InfoBoxTextBottom>
+            Probabilidade de emergência: <strong>2%</strong>
+          </InfoBoxTextBottom>
+          <ReportLink to={`/report/${moment().format('YYYY-MM-DD')}`}>
+            <Row type="flex" align="middle" justify="end">
+              <span>Ver relatório</span> <Icon type="right" style={{ fontSize: 10 }} />
+            </Row>
+          </ReportLink>
         </InfoBoxContainer>
       )
     }
