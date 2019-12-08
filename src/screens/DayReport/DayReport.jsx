58d09f752af7782235da66f60b61c21a7291b859
Wendel Nascimento <wendel.nascimento2@outlook.com>
2019-12-08 11:59:43 -0300

    feat: making DayReport dynamic also

--- src/screens/DayReport/DayReport.jsx
+++ src/screens/DayReport/DayReport.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
 import{ Icon } from 'antd';
+import moment from 'moment';
 
 import { ReportContainer, ReportTitle, ReportTitleDescription, ReportContent, CloseReport } from './style';
 
-const DayReport = () => (
-  <ReportContainer>
-    <CloseReport to="/map"><Icon type="close" /></CloseReport>
-    <ReportTitle>9/11</ReportTitle>
-    <ReportTitleDescription>
-      <p>
-        A chance de haver uma emergência no Porto nesse dia é de <span>2%</span><br/>
-      </p>
-      <p>
-        Os <span>21</span> indicadores apontam que o risco maior é no terminal <span>X</span>
-      </p>
-    </ReportTitleDescription>
-    <ReportContent>
-      <h3>Motivos</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia similique aliquid error suscipit beatae repellat autem delectus quis, earum officiis minus doloremque enim reprehenderit quod quidem ea fugiat? Tempora, ut.</p>
-    </ReportContent>
-    <ReportContent>
-      <h3>Entenda o cálculo</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque minus, quod velit soluta atque nostrum, iusto incidunt hic unde suscipit impedit enim provident, voluptatum quae ipsam quis? Autem, molestiae ullam.</p>
-    </ReportContent>
-    <ReportContent>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam dolores accusamus unde consequuntur eligendi. Soluta sunt quis eius iste corrupti ex ad sequi, accusamus placeat debitis laboriosam aliquam hic delectus.</ReportContent>
-  </ReportContainer>
-);
+import fallback from '../../fallback';
+
+const DayReport = ({ match }) => {
+  const day = fallback.find(item => item.date === match.params.date);
+
+  return (
+    <ReportContainer>
+      <CloseReport to="/map"><Icon type="close" /></CloseReport>
+      <ReportTitle>{moment(match.params.date).format('D/MM')}</ReportTitle>
+      <ReportTitleDescription>
+        <p>
+          A chance de haver uma emergência no Porto nesse dia é de <span>{day.dangerProbability}%</span><br />
+        </p>
+      </ReportTitleDescription>
+      <ReportContent>
+        <h3>Motivos</h3>
+        <p>Temperatura alta na esteira devido a carga de peso elevado:</p>
+        <span>Temperatura atual: {day.machineHealth.belt.temperature}ºC</span>
+        <span>Temperatura máxima: {day.machineHealth.belt.temperatureSupported}ºC</span>
+      </ReportContent>
+      <ReportContent>
+        <h3>Entenda o cálculo</h3>
+        <p>Através de milhares de variáveis e análise de dados históricos conseguimos ter acurácia para calcular potenciais riscos de emergência.</p>
+        <p>São utilizados desde dados meteorológicos, informações sobre periculosidade de cargas, informações sobre possíveis problemas conhecidos com fornecedores e até mesmo informações de sensores nos equipamentos do Porto.</p>
+      </ReportContent>
+    </ReportContainer>
+  );
+};
 
 export default React.memo(DayReport);
